e60bcc4d30b3f0efaca381c4fee8892b0fa512ec
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-11-21 20:44:07 -0300

    estilização dentro do javascript completa

--- 2° Semestre/Front-End Evandro/Aula 12/Proposta2/script.js
+++ 2° Semestre/Front-End Evandro/Aula 12/Proposta2/script.js
@@ -1,5 +1,40 @@
 let resolver = document.querySelector("input#resolver");
-let tabuada = document.querySelector("main");
+let tabuada = document.querySelector("div#tabuada");
+
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+
+let form = document.querySelector("form");
+let inputNumeroEscolhido = document.querySelector("input#numero_escolhido");
+
+// CSS
+header.style.display = "flex";
+header.style.justifyContent = "center";
+header.style.fontSize = "1.4em";
+header.style.textAlign = "center";
+
+main.style.display = "flex";
+main.style.flexDirection = "column";
+main.style.alignItems = "center";
+
+form.style.display = "flex";
+form.style.flexDirection = "column";
+form.style.border = "4px solid black";
+form.style.padding = "30px 20px";
+
+inputNumeroEscolhido.style.padding = "5px 8px";
+inputNumeroEscolhido.style.width = "400px";
+inputNumeroEscolhido.style.fontSize = "1.2em";
+inputNumeroEscolhido.style.backgroundColor = "#d1e8ff";
+inputNumeroEscolhido.style.border = "2px solid black";
+
+resolver.style.marginTop = "20px";
+resolver.style.fontSize = "1.4em"
+resolver.style.width = "420px";
+resolver.style.padding = "6px 0";
+
+tabuada.style.marginTop = "30px";
+tabuada.style.fontSize = "1.6em";
 
 resolver.onclick = function() {
     let numeroEscolhido = document.querySelector("input#numero_escolhido");
@@ -8,6 +43,6 @@ resolver.onclick = function() {
     }
     tabuada.innerHTML = "";
     for(let i = 1; i < 11 ; i++) {
-        tabuada.innerHTML += `${numeroEscolhido.value} x ${i} = ${Math.round(numeroEscolhido.value * i, 3)}<br>`
+        tabuada.innerHTML += `${numeroEscolhido.value} x ${i} = ${Math.round(numeroEscolhido.value * i)}<br>`
     }
 }
