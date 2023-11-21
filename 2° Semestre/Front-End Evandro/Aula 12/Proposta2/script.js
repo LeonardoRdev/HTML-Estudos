fe6da4ac03ccd00eb31e62e82b33a7c334c4777b
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-11-21 17:31:52 -0300

    Tabuada funcionando

--- 2° Semestre/Front-End Evandro/Aula 12/Proposta2/script.js
+++ 2° Semestre/Front-End Evandro/Aula 12/Proposta2/script.js
@@ -1,5 +1,13 @@
-resolver = document.querySelector("input#resolver");
+let resolver = document.querySelector("input#resolver");
+let tabuada = document.querySelector("main");
 
 resolver.onclick = function() {
-    alert("resolvido");
+    let numeroEscolhido = document.querySelector("input#numero_escolhido");
+    if(numeroEscolhido.value <= 0) {
+        numeroEscolhido.value = 1;
+    }
+    tabuada.innerHTML = "";
+    for(let i = 1; i < 11 ; i++) {
+        tabuada.innerHTML += `${numeroEscolhido.value} x ${i} = ${Math.round(numeroEscolhido.value * i, 3)}<br>`
+    }
 }
