0d04c83912f4496905997cad3bd9eec130c5ae6a
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-12-12 18:11:57 -0300

    Pasta 'Uninove' disponível novamente

---
+++ Uninove/2° Semestre/Front-End Evandro/Aula 04/código_aula4/index.js
@@ -0,0 +1,16 @@
+function showCurriculum(name, cur) {
+    document.getElementById("curriculum").innerHTML = cur;
+}
+
+/* A função acima faz a mesma coisa que a de baixo
+function showCurriculum(name) {
+    if(name=="gustavo") {
+        curriculumText = "Gustavo é garoto de programa"
+    } else if(name=="vitor") {
+        curriculumText = "Vitor dorme a toda hora"
+    } else if(name=="marcelo") {
+        curriculumText = "Marcelão joga mine como ninguém"
+    }
+    document.getElementById("curriculum").innerHTML = curriculumText
+}
+*/
---
+++ Uninove/README.md
@@ -0,0 +1,3 @@
+## Aqui vão ficar todos os meus estudos de HTML, CSS e JavaScript! 😁
+Os conteúdos estão todos separados por pastas!
+Ainda estou organizando os arquivos, mas está bem entendível por enquanto 🤠
