83440db195364cccd4d5f34919110191a9f7b93e
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-12-13 18:15:33 -0300

    Números aparecem na calculadora

---
+++ Meus Projetos/Calculadora/script.js
@@ -0,0 +1,76 @@
+let resultadoNumero = document.querySelector("#resultado_numero");
+
+// Aparentemente da pra fazer usando loop em um objeto, pesquisa depois.
+
+let botao1 = document.querySelector("#botao1");
+let botao2 = document.querySelector("#botao2");
+let botao3 = document.querySelector("#botao3");
+let botao4 = document.querySelector("#botao4");
+let botao5 = document.querySelector("#botao5");
+let botao6 = document.querySelector("#botao6");
+let botao7 = document.querySelector("#botao7");
+let botao8 = document.querySelector("#botao8");
+let botao9 = document.querySelector("#botao9");
+let botao10 = document.querySelector("#botao10");
+let botao11 = document.querySelector("#botao11");
+let botao12 = document.querySelector("#botao12");
+let botao13 = document.querySelector("#botao13");
+let botao14 = document.querySelector("#botao14");
+let botao15 = document.querySelector("#botao15");
+let botao16 = document.querySelector("#botao16");
+let botao17 = document.querySelector("#botao17");
+let botao18 = document.querySelector("#botao18");
+let botao19 = document.querySelector("#botao19");
+let botao20 = document.querySelector("#botao20");
+let botao21 = document.querySelector("#botao21");
+let botao22 = document.querySelector("#botao22");
+let botao23 = document.querySelector("#botao23");
+let botao24 = document.querySelector("#botao24");
+
+botao9.onclick = function() {
+    resultadoNumero.innerHTML = 7
+}
+
+botao10.onclick = function() {
+    resultadoNumero.innerHTML = 8
+}
+
+botao11.onclick = function() {
+    resultadoNumero.innerHTML = 9
+}
+
+botao13.onclick = function() {
+    resultadoNumero.innerHTML = 4
+}
+
+botao14.onclick = function() {
+    resultadoNumero.innerHTML = 5
+}
+
+botao15.onclick = function() {
+    resultadoNumero.innerHTML = 6
+}
+
+botao17.onclick = function() {
+    resultadoNumero.innerHTML = 1
+}
+
+botao18.onclick = function() {
+    resultadoNumero.innerHTML = 2
+}
+
+botao19.onclick = function() {
+    resultadoNumero.innerHTML = 3
+}
+
+botao21.onclick = function() {
+    resultadoNumero.innerHTML = "eita"
+}
+
+botao22.onclick = function() {
+    resultadoNumero.innerHTML = 0
+}
+
+botao23.onclick = function() {
+    resultadoNumero.innerHTML += ","
+}
