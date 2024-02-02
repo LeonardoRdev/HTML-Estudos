7943dcb4c7ce5537cb73fc5b0c80fbd298e3cf41
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-02-02 16:52:28 -0300

    Removendo sub-modulos e comentários da Logo Netflix

--- Meus Projetos/Netflix Logo/script.js
+++ Meus Projetos/Netflix Logo/script.js
@@ -1,22 +1,18 @@
-// let botao = document.querySelector("#mudarTema");
 let corpo = document.querySelector("body");
 let curvatura = document.querySelector(".item4");
 
 corpo.style.backgroundColor = "#fff";
 corpo.style.transition = "0.8s"
 curvatura.style.transition = "0.8s"
-// botao.style.border = "solid 2px black";
 
 corpo.onclick = () => {
     console.log(corpo.style.backgroundColor)
     if(corpo.style.backgroundColor == "rgb(255, 255, 255)") {
         corpo.style.backgroundColor = "#111";
         curvatura.style.backgroundColor = "#111"
-        // botao.style.border = "solid 2px white";
     }
     else {
         corpo.style.backgroundColor = "#fff";
         curvatura.style.backgroundColor = "#fff"
-        // botao.style.border = "solid 2px black";
     }
 }
