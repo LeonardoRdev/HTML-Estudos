d6b527512413bbc5872aed3f80483e7db7624201
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-02-23 19:11:16 -0300

    Aula 2 do #7DaysOfCode

---
+++ Alura/7DaysOfCode/dia_2/script.js
@@ -0,0 +1,21 @@
+let nome = prompt("Qual seu nome: ");
+let idade = prompt("Qual sua idade: ");
+let linguagem = prompt("Qual linguagem de programação você anda estudando: ");
+
+let gostaOuNao = prompt(`${nome}, você gosta de estudar ${linguagem}??
+1 para SIM
+2 para NÃO`);
+
+let textoTitulo = document.querySelector("#texto-titulo");
+
+if (gostaOuNao === 1) {
+    textoTitulo.innerHTML = `Olá ${nome}, você tem ${idade} anos e já está apredendo ${linguagem}! (e ainda gosta dela!)`;
+}
+
+else if (gostaOuNao === 2) {
+    textoTitulo.innerHTML = `Olha ${nome}, procure outra linguagem que você goste mais que ${linguagem}, por que você ainda tem ${idade} e ainda dá tempo!`
+}
+
+else {
+    textoTitulo.innerHTML = `${nome}, as opções eram ou 1 ou 2, não tem outro jeito, agora dá F5 e refaz tudo`
+}
