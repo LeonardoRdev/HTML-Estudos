cefe37c0d67c9fdbf97a9d285b948963dc0bc8cf
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-02-28 18:22:56 -0300

    Dia 4 finalizado

--- Alura/7DaysOfCode/dia_4/script.js
+++ Alura/7DaysOfCode/dia_4/script.js
@@ -1,5 +1,42 @@
-botaoChutar = document.querySelector("#enviar");
+let numeroAleatorio = Math.ceil(Math.random() * 10);
+let tentativasUsadas = 0;
 
-botaoChutar.addEventListener("click", function() {
-    alert("enviou")
+let mensagemTentativasRestastes = document.querySelector("#msg-tentativas-restantes");
+const botaoChutar = document.querySelector("#enviar");
+const inputChutar = document.querySelector("#chute");
+let resultadoChute = document.querySelector("#resultado-chute");
+
+botaoChutar.addEventListener("click", function () {
+    if (tentativasUsadas < 3) {
+        tentativasUsadas++;
+        
+
+        let valorChutado = inputChutar.value;
+        if (valorChutado == numeroAleatorio) {
+            resultadoChute.innerHTML = "Deu Bom! Você acertou!!";
+            resultadoChute.style.color = "#1eeb4e"
+            tentativasUsadas = 3;
+            return;
+        }
+        else {
+            if (valorChutado > numeroAleatorio) {
+                resultadoChute.innerHTML = "Chutou alto...";
+            }
+            else if (valorChutado < numeroAleatorio && valorChutado != "") {
+                resultadoChute.innerHTML = "Chutou baixo...";
+            }
+            else {
+                resultadoChute.innerHTML = "Por favor, apenas números!";
+                tentativasUsadas--;
+            }
+        }
+
+        mensagemTentativasRestastes.innerHTML = `${tentativasUsadas}/3 tentativas restantes`
+
+        if (tentativasUsadas >= 3) {
+            resultadoChute.innerHTML = `ACABARAM AS CHANCES<br>o número era ${numeroAleatorio}`;
+            resultadoChute.style.color = "#f00";
+        }
+        inputChutar.value = "";
+    }
 })
