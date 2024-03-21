0ed31010e51f4d1f2d83b1d66c5b3115bbe3efca
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-03-21 17:13:32 -0300

    Lição de casa | Jogo da Forca -> em andamento

--- Uninove/3° Semestre/Estrutura de Dados/Aula 04/ex_6.c
+++ Uninove/3° Semestre/Estrutura de Dados/Aula 04/ex_6.c
@@ -10,17 +10,31 @@ digitar a palavra de uma só vez, porém se errar perde o jogo.
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    char palavraSecreta[20], chutePalavra[20];
+    char palavraSecreta[20];
+    char chutePalavra[20];
 
     printf("[JOGADOR 1]:\nDigite a palavra secreta: ");
-    scanf("%[^\n]", &palavraSecreta);
+    scanf(" %[^\n]", &palavraSecreta);
 
-    printf("\nA palavra secreta é: %s", palavraSecreta);
+    system("cls");
+    
+    printf("[JOGADOR 2]:\nChute uma LETRA ou a PALAVRA INTEIRA\n*OBS: chutar a palavra inteira consome todas as chances\n\nChute: ");
+    scanf(" %s", &chutePalavra);
+
+    printf("\nPALAVRA SECRETA: %s\nCHUTE PALAVRA: %s", palavraSecreta, chutePalavra);
+
+    if(strstr(palavraSecreta, chutePalavra) != NULL) {
+        printf("\nTEM ISSO NA PALAVRA!");
+        // O problema atual é que cada caractere do char "palavraSecreta" precisa ser um item de uma lista,
+        // e isso vai dar um trabalho.
+        printf(strstr(palavraSecreta, chutePalavra));
+    }
 
     // FAZER EM CASA:
     //  [https://pt.stackoverflow.com/questions/95977/jogo-da-forca-com-funções-em-c](https://pt.stackoverflow.com/questions/95977/jogo-da-forca-com-fun%C3%A7%C3%B5es-em-c)
