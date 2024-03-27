650a63558833ccda92a9a31384eb96d456beb34e
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-03-27 18:19:29 -0300

    Interface inicial feita

--- Meus Projetos/Jogo_da_Forca/script.js
+++ Meus Projetos/Jogo_da_Forca/script.js
@@ -1,9 +1,23 @@
+// Botão JOGAR
+const botaoJogar = document.querySelector("#botao-jogar");
+const telaInicial = document.querySelector("main #tela-inicial");
+const interfaceJogo = document.querySelector("main #interface-jogo");
+botaoJogar.addEventListener("click", () => {
+    telaInicial.style.display = "none";
+    interfaceJogo.style.display = "flex";
+});
+
+
+
+
+// Para o form não atualizar a página
 const form = document.querySelector("form");
 form.addEventListener('submit', e => {
     e.preventDefault();
 });
 
-const enviar = document.querySelector("#enviar");
-enviar.addEventListener("click", () => {
+// Botão enviar (chute)
+const botaoEnviar = document.querySelector("#enviar");
+botaoEnviar.addEventListener("click", () => {
     alert("enviado");
-})
+});
