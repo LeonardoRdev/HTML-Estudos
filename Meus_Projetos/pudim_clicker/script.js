b7a63eaf492d04850146f9847cf3313b92b3c20a
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-07-31 23:56:08 -0300

    Custo dos upgrades

--- Meus_Projetos/pudim_clicker/script.js
+++ Meus_Projetos/pudim_clicker/script.js
@@ -4,6 +4,28 @@ let quantidadePudins = document.querySelector("#quantidade_pudins")
 let pudins = 0;
 let poderDoClique = 1;
 
+let precoUpgradeConfeiteira = 0;
+let precoUpgradeChef = 0;
+let precoUpgradePadaria = 0;
+let precoUpgradeConfeitaria = 0;
+let precoUpgradeSupermercado = 0;
+let precoUpgradeCafeteria = 0;
+let precoUpgradeGourmet = 0;
+
+let divCustoUpgrade = document.querySelectorAll(".custo_upgrade");
+
+let upgrade1 = divCustoUpgrade[0].querySelector(`p`);
+let upgrade2 = divCustoUpgrade[1].querySelector(`p`);
+let upgrade3 = divCustoUpgrade[2].querySelector(`p`);
+let upgrade4 = divCustoUpgrade[3].querySelector(`p`);
+let upgrade5 = divCustoUpgrade[4].querySelector(`p`);
+let upgrade6 = divCustoUpgrade[5].querySelector(`p`);
+let upgrade7 = divCustoUpgrade[6].querySelector(`p`);
+
+upgrade1.addEventListener("click", () => {
+    console.log("Clique no upgrade 1 detectado");
+});
+
 divImagemPudins.addEventListener("click", () => {
     pudins += poderDoClique;
 
