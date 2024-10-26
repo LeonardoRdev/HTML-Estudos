15b281bb7e346970ee1f8d00b9fa200a325596de
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-10-25 21:51:46 -0300

    Ordem das respostas aleatórias | Quiz Halloween

--- Meus_Projetos/quiz_halloween/script.js
+++ Meus_Projetos/quiz_halloween/script.js
@@ -1,7 +1,13 @@
-respostas = document.querySelectorAll(".resposta");
+const divComRespostas = document.querySelector("#respostas");
+const  respostas = document.querySelectorAll(".resposta");
+let arrayRespostas = [];
 
-respostas.forEach((resposta) => {
-    console.log(resposta)
+respostas.forEach((resposta, index) => {
+
+    // Aleatoriza a ordem das respostas
+    arrayRespostas[index] = resposta;
+
+    // Função ao clicar em alguma resposta
     resposta.onclick = () => {
 
         // Caso acertar a resposta
@@ -14,4 +20,31 @@ respostas.forEach((resposta) => {
             resposta.style.backgroundColor = "#f00B"
         }
     }
-})
+});
+
+
+// Função para aleatorizar a ordem das respostas
+function aleatorizarOrdemRespostas() {
+
+    let novaOrdemRespostas = [];
+    let tamanhoArrayRespostas = arrayRespostas.length;
+
+    for (let i = 0; i < tamanhoArrayRespostas; i++) {
+    
+        // Pega um item aleatório do array original, e passa para um novo array
+        let indiceEscolhido = Math.floor(Math.random() * arrayRespostas.length);
+        novaOrdemRespostas.push(arrayRespostas[indiceEscolhido]);
+        arrayRespostas.splice(indiceEscolhido, 1);
+    }
+
+    // Limpa a div que contém as respostas (sem estarem embaralhadas)
+    divComRespostas.innerHTML = "";
+
+    // Adiciona as respostas com a nova ordem (aleatória)
+    novaOrdemRespostas.forEach((resposta) => {
+        divComRespostas.appendChild(resposta);
+    })
+
+}
+
+aleatorizarOrdemRespostas();
