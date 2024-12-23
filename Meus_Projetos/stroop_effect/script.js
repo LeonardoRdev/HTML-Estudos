73dec66937b35b39b7a6b96e868798cee9b38d71
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-12-23 20:02:58 -0300

    Botão de ajuda agora ajuda! Stroop Effect

--- Meus_Projetos/stroop_effect/script.js
+++ Meus_Projetos/stroop_effect/script.js
@@ -1,11 +1,14 @@
 // FAZER:
 // Ao clicar em qualquer botão, iniciar o jogo. Trocar a seta de voltar página por uma de recarregar a página
 // O primeiro timer do intervalo não mostra nenhuma palavra, só depois do "tempoEntrePalavras" passar.
+// Estilização: radiante circular com cinza no meio e branco dos lados
+// "reroll" caso a próxima palavra tenha o mesmo nome e mesma cor da anterior (usar while)
 
+// Variáveis
 const setaVoltarPagina = document.querySelector("#seta-voltar-pagina");
 const headerConfiguracoes = document.querySelector("header");
-
-console.log(Math.floor(Math.random() * 3));
+const botaoAjuda = document.querySelector("#botao-configuracao-ajuda");
+const divMenuAjuda = document.querySelector("#menu-de-ajuda");
 
 // Lista com cores que apareceram (tanto seus nomes quantos as respectivas cores em hexadecimal)
 const listaNomeCores = [
@@ -15,15 +18,19 @@ const listaNomeCores = [
     "VERDE",
     "ROXO",
     "AZUL",
+    "ROSA",
+    "MARROM"
 ];
 
 const listaHexadecimalCores = [
     "#ff1212",  // Vermelho
     "#faf32a",  // Amarelo
     "#000",     // Preto
-    "##3cdb37", // Verde
+    "#3cdb37", // Verde
     "#ab29d6",  // Roxo
-    "#19c1e3"   // Azul
+    "#0c7ded",  // Azul
+    "#ff0084",  // Rosa
+    "#572d15"   // Marrom
 ];
 
 // Botões de dificuldade
@@ -33,38 +40,39 @@ const botaoDificuldadeMedio = document.querySelector("#botao-dificuldade-medio")
 const botaoDificuldadeDificil = document.querySelector("#botao-dificuldade-dificil");
 
 botaoDificuldadeFacil.onclick = () => {
-    iniciarJogo(3);
+    iniciarJogo(3, 15);
 }
 
 botaoDificuldadeMedio.onclick = () => {
-    iniciarJogo(1);
+    iniciarJogo(1, 25);
 }
 
 botaoDificuldadeDificil.onclick = () => {
-    iniciarJogo(0.5);
+    iniciarJogo(0.5, 40);
 }
 
 // Função que inicia o "jogo"
-function iniciarJogo(tempoEntrePalavras) {
+function iniciarJogo(tempoEntrePalavras, quantidadePalavras) {
     // Ir para a tela do jogo
     telaDoJogo(true);
 
     let repeticoes = 0;
+    const palavraDaVez = document.querySelector("#palavra-da-vez");
+    palavraDaVez.innerHTML = "PREPARE-SE"
+
 
     // Inicia o intervalo com as palavras
     const intervalo = setInterval(() => {
         // Lógica para alterar as palavras e cores
-        const palavraDaVez = document.querySelector("#palavra-da-vez");
         palavraDaVez.innerHTML = listaNomeCores[Math.floor(Math.random() * listaNomeCores.length)];
         palavraDaVez.style.color = listaHexadecimalCores[Math.floor(Math.random() * listaHexadecimalCores.length)];
 
         // Contagem das repetições
         repeticoes++;
         console.log(`Repetições ${repeticoes}`)
-        // console.log(`Passou ${tempoEntrePalavras} segundo(s)`);
 
-        // Termina o intervalo após 10 repetições
-        if (repeticoes >= 10) {
+        // Termina o intervalo após x repetições
+        if (repeticoes >= quantidadePalavras) {
             clearInterval(intervalo);
             palavraDaVez.innerHTML = "";
             telaDoJogo(false);
@@ -74,7 +82,7 @@ function iniciarJogo(tempoEntrePalavras) {
 }
 
 function telaDoJogo(entrarNoJogo) {
-    // Se for iniciar o jogo
+    // Se for iniciar o jogo, esconder a tela inicial
     if (entrarNoJogo) {
         divConfiguracaoDificuldade.style.display = "none";
         setaVoltarPagina.style.display = "none";
@@ -86,5 +94,20 @@ function telaDoJogo(entrarNoJogo) {
         divConfiguracaoDificuldade.style.display = "flex";
         setaVoltarPagina.style.display = "block";
         headerConfiguracoes.style.display = "block";
+    }
+}
+
+// Botão de ajuda "?"
+botaoAjuda.onclick = () => {
+    // Abrir menu
+    if (divMenuAjuda.style.display == "flex") {
+        divMenuAjuda.style.display = "none";
+        divConfiguracaoDificuldade.style.display = "flex";
+    }
+
+    // Fechar menu
+    else {
+        divMenuAjuda.style.display = "flex";
+        divConfiguracaoDificuldade.style.display = "none";
     }
 }
