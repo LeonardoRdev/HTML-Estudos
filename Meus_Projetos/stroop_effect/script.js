a883b43333b6b54978b2a394dce13fe2499876db
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-01-02 18:48:58 -0300

    Menu com bandeiras consertado e funcionando | Stroop Effect

--- Meus_Projetos/stroop_effect/script.js
+++ Meus_Projetos/stroop_effect/script.js
@@ -1,8 +1,10 @@
 // FAZER:
-// Ao clicar em qualquer botão, iniciar o jogo. Trocar a seta de voltar página por uma de recarregar a página
-// O primeiro timer do intervalo não mostra nenhuma palavra, só depois do "tempoEntrePalavras" passar.
-// Estilização: radiante circular com cinza no meio e branco dos lados
-// "reroll" caso a próxima palavra tenha o mesmo nome e mesma cor da anterior (usar while)
+// Ao clicar em qualquer botão, iniciar o jogo. Trocar a seta de voltar página por uma de recarregar a página;
+// O primeiro timer do intervalo não mostra nenhuma palavra, só depois do "tempoEntrePalavras" passar;
+// Estilização: radiante circular com cinza no meio e branco dos lados;
+// "reroll" caso a próxima palavra tenha o mesmo nome e mesma cor da anterior (usar while);
+// Agora falta realmente trocar o idioma da página.
+
 
 // Variáveis
 const setaVoltarPagina = document.querySelector("#seta-voltar-pagina");
@@ -131,64 +133,67 @@ const elementoBandeira = {};
 listaBandeiras.forEach((bandeira) => {
     elementoBandeira[bandeira] = document.querySelector(`#bandeira-${bandeira}`);
     elementoBandeira[bandeira].onclick = () => {
-        clicarNaBandeira(bandeira);
-        console.log(`===\n`)
+        clicarNaBandeira(elementoBandeira[bandeira]);
+        console.log(`=-==-==-=-==-==-=-==\n`)
     };
-    
-    // console.log(elementoBandeira[bandeira])
 });
 
-function clicarNaBandeira(bandeiraClicada) {
-    console.log(`Bandeira clicada = ${bandeiraClicada}`)
-    // Pega o nome da bandeira atual pelo ID
-    let elementoBandeiraAtual = document.querySelector(".bandeira-atual");
-    let nomeBandeiraAtual = elementoBandeiraAtual.id.split("-")[1];
 
-    let bandeiras = document.querySelectorAll(".bandeiras");
-    let translateY = 75;
-
-    // Caso não clique na bandeira já escolhida (caso escolha outra língua)
-    if (nomeBandeiraAtual != bandeiraClicada) {
-        console.log("NOVA BANDEIRA ESCOLHIDA");
-        elementoBandeiraAtual.classList.remove("bandeira-atual");
-        elementoBandeira[bandeiraClicada].classList.add("bandeira-atual");
-        // elementoBandeira[bandeiraClicada].style.transform = `TranslateY(${0}px)`;
+function clicarNaBandeira(elementoBandeiraClicada) {
+    // *fechar menu
+    if (menuLinguasAberto === true) {
+        // Se o menu estiver aberto, fechar o menu e selecionar a língua escolhida
+        menuLinguasAberto = false;
+        escolherNovaLingua(elementoBandeiraClicada);
+        fecharMenuLinguas();
     }
-
-    // Caso escolha a bandeira já selecionada (caso escolha a mesma língua)
+    
+    // *abrir menu
     else {
-        console.log("Selecionou a bandeira atual")
-        // Abrir menu
-        if (menuLinguasAberto === false) {
-            console.log("Abriu o menu")
-            menuLinguasAberto = true;
-
-            // Animação de mostrar as bandeiras
-            elementoBandeira[bandeiraClicada].style.transform = `TranslateY(${0}px)`;
-
-            bandeiras.forEach((bandeira) => {
-                // Caso você tenha não tenha clicado na língua atual (abrir menu)
-                if (!bandeira.classList.contains("bandeira-atual")) {
-                    bandeira.style.opacity = "1";
-                    bandeira.style.transform = `TranslateY(${translateY}px)`;
-                    translateY += 75;
-                }
-            });
-        }
-
-        // Fechar menu
-        else {
-            console.log("Fechou o menu")
-            menuLinguasAberto = false;
-            bandeiras.forEach((bandeira) => {
-                // Caso você tenha não tenha clicado na língua atual (abrir menu)
-                if (!bandeira.classList.contains("bandeira-atual")) {
-                    bandeira.style.opacity = "0";
-                    bandeira.style.transform = `TranslateY(${0}px)`;
-                }
-            });
-        }
+        // Se o menu estiver fechado, abrir o menu com as línguas.
+        menuLinguasAberto = true;
+        abrirMenuLinguas();
     }
 
-    console.log("Variável menu aberto = " + menuLinguasAberto)
+    console.log(`Menu aberto? -> ${menuLinguasAberto}`)
+}
+
+
+// FUNÇÃO FECHAR MENU LÍNGUAS
+function fecharMenuLinguas() {
+    let bandeiras = document.querySelectorAll(".bandeiras");
+    let translateY = 0;
+    
+    bandeiras.forEach((bandeira) => {
+        // Esconder as bandeiras
+        bandeira.style.opacity = "0";
+        bandeira.style.transform = `TranslateY(${translateY}px)`;
+    });
+
+    // Exibir somente a bandeira escolhida
+    let elementoBandeiraAtual = document.querySelector(".bandeira-atual");
+    elementoBandeiraAtual.style.opacity = "1";
+}
+
+
+// FUNÇÃO ABRIR MENU LÍNGUAS
+function abrirMenuLinguas() {
+    let bandeiras = document.querySelectorAll(".bandeiras");
+    let translateY = 0;
+
+    bandeiras.forEach((bandeira) => {
+        // Exibir todas as bandeiras
+        bandeira.style.opacity = "1";
+        bandeira.style.transform = `TranslateY(${translateY}px)`;
+        translateY += 75;
+    });
+}
+
+
+// FUNÇÃO DE ESCOLHER OUTRO IDIOMA
+function escolherNovaLingua(elementoBandeiraEscolhida) {
+    let elementoBandeiraAtual = document.querySelector(".bandeira-atual");
+    console.log(`Elemento: ${elementoBandeiraEscolhida}`)
+    elementoBandeiraAtual.classList.remove("bandeira-atual");
+    elementoBandeiraEscolhida.classList.add("bandeira-atual");
 }
