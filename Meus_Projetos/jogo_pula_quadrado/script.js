6a41b799e84d2deeca68985026d2699fd486839e
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-01-23 19:45:16 -0300

    O quadrado pulante agora pula no comando! | Jogo pula quadrado

---
+++ Meus_Projetos/jogo_pula_quadrado/script.js
@@ -0,0 +1,22 @@
+// Se segurar pra pular, ele demora aqueles 1 seg antes de pular infinito, resolver.
+
+window.onkeydown = (teclaPressionada) => {
+    // O pulo pode ser iniciado clicar "Espaço, W, Z, ou seta pra cima"
+    if (teclaPressionada.code === "Space" || teclaPressionada.code === "KeyW" || teclaPressionada.code === "ArrowUp" || teclaPressionada.code === "KeyZ") {
+
+        const personagem = document.querySelector("#personagem");
+
+        // Iniciar animação de pulo somente se o personagem já não estiver pulando
+        if (!personagem.classList.contains("animacaoPular")) {
+            personagem.classList.add("animacaoPular");
+
+            intervaloDoPulo = setInterval(() => {
+                // Remove a animação de pular após o tempo dela acabar (0.75s)
+                personagem.classList.remove("animacaoPular");
+                clearInterval(intervaloDoPulo);
+            }, 750);
+
+        }
+
+    }
+}
