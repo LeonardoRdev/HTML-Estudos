8835e8a55088bb9c07d268871c3d3ba6e68bb2af
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-01-27 20:35:21 -0300

    Obstaculos param de vir quando atingidos | Jogo Pula Quadrado

--- Meus_Projetos/jogo_pula_quadrado/script.js
+++ Meus_Projetos/jogo_pula_quadrado/script.js
@@ -1,7 +1,9 @@
 // Se segurar pra pular, ele demora aqueles 1 seg antes de pular infinito, resolver.
 
+const divJogo = document.querySelector("#jogo");
 const personagem = document.querySelector("#personagem");
 const obstaculo = document.querySelector("#obstaculo");
+obstaculo.classList.add("animacaoDeslizar");
 
 window.onclick = () => {
     pularPersonagem();
@@ -29,7 +31,9 @@ function pularPersonagem() {
 
 }
 
-setInterval(() => {
+let intervaloDoDeslizamento = setInterval(() => {
+    let divJogoX = divJogo.getBoundingClientRect().left;
+    
     let personagemY = personagem.getBoundingClientRect().top;
     let personagemX = personagem.getBoundingClientRect().left;
     let personagemWidth = personagem.getBoundingClientRect().width;
@@ -37,15 +41,24 @@ setInterval(() => {
 
     let obstaculoY = obstaculo.getBoundingClientRect().top;
     let obstaculoX = obstaculo.getBoundingClientRect().left;
+    let obstaculoWidth = obstaculo.getBoundingClientRect().width;
+
 
     // Se encostar no obstaculo (tanto por cima quanto dos lados)
     if (obstaculoX < personagemX + personagemWidth && obstaculoX > personagemX - personagemWidth && parseFloat(personagemY + personagemHeight) > obstaculoY) {
+        // "pausa" o jogo no frame da derrota
         personagem.style.backgroundColor = "black";
+        obstaculo.classList.remove("animacaoDeslizar");
+        obstaculo.style.left = `${obstaculoX - divJogoX + obstaculoWidth - 2}px`; // Esse "- 2" é por conta da hitbox que tem alguns px a mais do que aparenta, então ficaria um espaço em branco meio paia
+
+        // Parar de enviar obstaculos
+        clearInterval(intervaloDoDeslizamento);
     }
     
     // Caso não esteja enconstando (serve basicamente pra resetar a cor pra padrão após)
     else {
+        // (não serve mais por conta do ClearInterval() ao encostar no obstaculo, ainda vou ver se vou precisar disso mais tarde)
         personagem.style.backgroundColor = "brown";
     }
 
-}, 10)
+}, 1);
