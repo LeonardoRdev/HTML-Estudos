1e3e5e042ce93b3176fb7a8463ec76c0fa0171b1
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2025-02-03 22:32:40 -0300

    Aleatoriedade na velocidade dos obstaculos

--- Meus_Projetos/jogo_pula_quadrado/script.js
+++ Meus_Projetos/jogo_pula_quadrado/script.js
@@ -1,6 +1,9 @@
+// BUG: Se você clicar muito desesperadamente pra reiniciar o jogo, o boneco fica meio que resetando o pulo.
+
 // Ao reiniciar jogo, clicar na seta de reiniciar faz o personagem já começar pulando.
 // Se segurar pra pular, ele demora aqueles 1 seg antes de pular infinito, resolver.
-// Dificultar o jogo a cada x segundos.
+// Dificultar o jogo a cada x segundos. Fazer ser aleatório, replayability
+// Colocar moedas aleatórias?
 // Adicionar sons.
 
 const pontos = document.querySelector("#pontos");
@@ -22,6 +25,10 @@ window.onclick = () => {
 }
 
 window.onkeydown = (teclaPressionada) => {
+    if (setaRecarregar.style.display == "block") {
+        recarregarJogo();
+    }
+
     // O pulo pode ser iniciado clicar "Espaço, W, Z, ou seta pra cima"
     if (teclaPressionada.code === "Space" || teclaPressionada.code === "KeyW" || teclaPressionada.code === "ArrowUp" || teclaPressionada.code === "KeyZ") {
         pularPersonagem();
@@ -30,11 +37,12 @@ window.onkeydown = (teclaPressionada) => {
 
 function pularPersonagem() {
     if (!gameOver) { // Caso o jogo ainda não tenha acabado
+        console.log("RODANDO PULO.........")
         // Iniciar animação de pulo somente se o personagem já não estiver pulando
         if (!personagem.classList.contains("animacaoPular")) {
             personagem.classList.add("animacaoPular");
 
-            intervaloDoPulo = setInterval(() => {
+            let intervaloDoPulo = setInterval(() => {
                 // Remove a animação de pular após o tempo dela acabar (0.75s)
                 personagem.classList.remove("animacaoPular");
                 clearInterval(intervaloDoPulo);
@@ -47,9 +55,20 @@ function pularPersonagem() {
 iniciarJogo();
 
 function iniciarJogo() {
+    // DIFICULDADES:
+    let velocidadeObstaculoRapido = "deslizarObstaculo 0.8s linear";        // Padrão
+    let velocidadeObstaculoPadrao = "deslizarObstaculo 2s linear";          // Rápido
+    let velocidadeObstaculoPadraoRapido = "deslizarObstaculo 1.4s linear";  // Devagar
+    let velocidadeObstaculoDevagar = "deslizarObstaculo 3s linear";         // Muito Devagar
+    let velocidadeObstaculoMuitoDevagar = "deslizarObstaculo 4s linear";    // Quase pouco rápido
+
+    let listaVelocidades = [velocidadeObstaculoRapido, velocidadeObstaculoPadrao, velocidadeObstaculoPadraoRapido, velocidadeObstaculoDevagar, velocidadeObstaculoMuitoDevagar]; // Lista com os "leveis" (cada level muda a velocidade dos obstaculos)
+    // let velocidadAleatoria = Math.floor(Math.random() * listaVelocidades.length);
+    // let velocidadeObstaculo = listaVelocidades[velocidadAleatoria];
+
     let variavelPulosBemSucedidos = 0;
     pontos.innerHTML = `${variavelPulosBemSucedidos}`; // Reseta a pontuação pra 0;
-    classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 2s linear";
+    classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 2s linear"; // O primeiro obstaculo é sempre igual
 
     let intervaloDoDeslizamento = setInterval(() => {
 
@@ -57,34 +76,47 @@ function iniciarJogo() {
             variavelPulosBemSucedidos++;
             pontos.innerHTML = `${variavelPulosBemSucedidos}`; // Aumenta a pontuação a cada pulo pulado (kkkkk)
             console.log(`Pulos bem sucedidos: ${variavelPulosBemSucedidos}`);
-            
+
             classeAnimacaoDeslizar.style.animation = "none";
             classeAnimacaoDeslizar.offsetWidth; // Força o navegador a reconhecer que houve uma mudança de estilo, garantindo que a animação reinicie corretamente. 
 
             // Vai alterando a velocidade do obstaculo conforme o tempo for passando
 
-            if (variavelPulosBemSucedidos < 4) { // Muda a velocidade do obstaculo até a condição não ser verdadeira
-                classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 2s linear";
+            // PROBLEMA: CADA OBSTACULO ESTÁ ATUALMENTE VINDO ALEATÓRIO, JÁ QUE A CADA EXECUÇÃO É ALEATORIZADO UMA NOVA VELOCIDADE
+            if (variavelPulosBemSucedidos < 2) { // Muda a velocidade do obstaculo até a condição não ser verdadeira
+                let velocidadAleatoria = Math.floor(Math.random() * listaVelocidades.length);
+                let velocidadeObstaculo = listaVelocidades[velocidadAleatoria];
+
+                classeAnimacaoDeslizar.style.animation = velocidadeObstaculo;
             }
-            
+
             else if (variavelPulosBemSucedidos < 10) {
-                classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 1s linear";
+                let velocidadAleatoria = Math.floor(Math.random() * listaVelocidades.length);
+                let velocidadeObstaculo = listaVelocidades[velocidadAleatoria];
+
+                classeAnimacaoDeslizar.style.animation = velocidadeObstaculo;
             }
 
             else if (variavelPulosBemSucedidos < 13) {
-                classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 3s linear";
+                let velocidadAleatoria = Math.floor(Math.random() * listaVelocidades.length);
+                let velocidadeObstaculo = listaVelocidades[velocidadAleatoria];
+
+                classeAnimacaoDeslizar.style.animation = velocidadeObstaculo;
             }
 
             else if (variavelPulosBemSucedidos < 14) {
-                classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 4s linear";
+                let velocidadAleatoria = Math.floor(Math.random() * listaVelocidades.length);
+                let velocidadeObstaculo = listaVelocidades[velocidadAleatoria];
+
+                classeAnimacaoDeslizar.style.animation = velocidadeObstaculo;
             }
 
-            else if (variavelPulosBemSucedidos >= 14) { // Continua infinitamente
+            else if (variavelPulosBemSucedidos >= 14) { // Continua infinitamente em uma boa velocidade
                 classeAnimacaoDeslizar.style.animation = "deslizarObstaculo 1.5s linear";
             }
         }
-        
-        
+
+
         let divJogoX = divJogo.getBoundingClientRect().left;
         let divJogoY = divJogo.getBoundingClientRect().top;
 
